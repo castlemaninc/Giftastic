e0c268fe9e87d9daa722306e14d923b2e28e4f48
Bryan Castleman <Bryan Castleman>
2017-03-12 11:07:02 -0700

    new buttons generate and now also create new gifs on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,24 @@
 
-
-
+// GIFTASTIC! 
 var topics = ["Larry Bird", "Shaquille O'Neal", "Kobe Bryant", "Stephen Curry", "Spud Webb", "Yao Ming", "Kevin Durant", "Kevin Garnett", "Michael Jordan", "Kareem Abdul Jabbar", "Chris Paul", "Dwayne Wade", "Dwight Howard"];
 
 var button;
+var newTopic = ""; // new topic that will be added via the input field 
+
+// function to create new buttons from the topics array
+var buttonGenerator = function (){
+	// the previous div elements are emptied 
+	 $("#buttonArea").empty();
+	// loops through the array and creates buttons
+	for(i = 0; i < topics.length; i++) {
+		button = $("<button type=" + "button" + ">" + topics[i] + "</button>").addClass("btn btn-warning").attr("data",topics[i]);
+		$("#buttonArea").append(button);
+	};
+}
 
 
-for(i = 0; i < topics.length; i++) {
-     button = $("<button type=" + "button" + ">" + topics[i] + "</button>").addClass("btn btn-warning").attr("data",topics[i]);
-     $("#buttonArea").append(button);
-     
-};
-
-
-  // 3.When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page. 
-
-$(".btn-warning").on("click", function(){
+// The user clicks on a generated orange button, which generates 10 static, non-animated gif images from the GIPHY API and places them on the page. 
+$("#buttonArea").on("click", ".btn", function(){
   		var thing = $(this).attr("data");
   		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + thing + "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -31,81 +34,71 @@ $(".btn-warning").on("click", function(){
           	var results = response.data;
 
           	for (var i = 0; i < results.length; i++) {
+          		// a div is created to hold a gif of any topic
+	          	var topicDiv = $("<div>");
+	 			
+	          	// Under every gif, display its rating (PG, G, so on).
+	 			var p = $("<p>");
+	 			p.text(results[i].rating);
+	 			var p = $("<p>").text("Rating: " + results[i].rating);
 
-          	var topicDiv = $("<div>");
- 			var p = $("<p>");
+	 			// add a CSS style to create colored borders around the gifs
+	 			var topicImage = $("<img>").addClass("orangeBorder");
 
- 			p.text(results[i].rating);
-
- 			var p = $("<p>").text("Rating: " + results[i].rating);
-
- 			var topicImage = $("<img>").addClass("orangeBorder");
-
- 			topicImage.attr("src", results[i].images.fixed_height_still.url);
- 			topicImage.attr("data-still", results[i].images.fixed_height_still.url);
- 			topicImage.attr("data-animate", results[i].images.fixed_height.url)
- 			topicImage.attr("data-state", "still")
- 			topicImage.addClass("gif");
- 			
- 			// add still version of the image 
-
- 			topicDiv.append(topicImage);
-
- 			topicDiv.append(p); 			
-
- 			$("#gifArea").prepend(topicDiv);
-
+	 			// add states of animate and still which will be toggled 
+	 			topicImage.attr("src", results[i].images.fixed_height_still.url);
+	 			topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+	 			topicImage.attr("data-animate", results[i].images.fixed_height.url)
+	 			topicImage.attr("data-state", "still")
+	 			topicImage.addClass("gif");
+	 			
+	 			// image is appended to the div
+	 			topicDiv.append(topicImage);
+	 			// rating is appended to the div below the gif
+	 			topicDiv.append(p); 			
+	 			// new images will be placed at the beginning (top) of the containing gif area
+	 			$("#gifArea").prepend(topicDiv);
  			}
   		})
   })
 
-  
 
-  // 4.When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+// When the user clicks one of the still GIPHY images, and it animates. When the user clicks the gif again, it stops playing.
+$("#gifArea").on("click", ".gif", function(event){
+	event.preventDefault();
+	
+	// gets the current state of the clicked gif 
+	var state = $(this).attr("data-state");
+	
+	// according to the current state gifs toggle between animate and still 
+	if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
 
-  $("#gifArea").on("click", ".gif", function(event){
-  	event.preventDefault();
-  	
+})
+   
 
-  	var state = $(this).attr("data-state");
-  	console.log(state);
-  	if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
+// The form takes the value from the input box and adds it into the topics  array. The buttonGenerator function is called that takes each topic in the array remakes the buttons on the page.
 
-  })
 
-  
-  // 5.Under every gif, display its rating (PG, G, so on).
+$(".submit").on("click", function(event){
+	event.preventDefault();
 
-  // 6.Add a form to your page takes the value from a user input box and adds it into your  topics  array. Then make a function call that takes each topic in the array remakes the buttons on the page.
-
-  var newTopic = "";
-
-  $(".submit").on("click", function(event){
-  	event.preventDefault();
-  	console.log("submit");
-
-  	newTopic = $("#topic-input").val();
-
-  	topics.push(newTopic);
-  	console.log(topics);
-  	buttonGenerator();
-  });
+	console.log("submit");
+	// sets inputted value to newTopic 
+	newTopic = $("#topic-input").val();
+	// new topic is added to the topics array 
+	topics.push(newTopic);
+	console.log(topics);
+	// call the function that creates the new button
+	buttonGenerator();
+});
 
 
 
-  var buttonGenerator = function (){
-  	 $("#buttonArea").empty();
-	 for(i = 0; i < topics.length; i++) {
-     button = $("<button type=" + "button" + ">" + topics[i] + "</button>").addClass("btn btn-warning").attr("data",topics[i]);
-     $("#buttonArea").append(button);
-     
-  };
-}
-
+buttonGenerator();
  
